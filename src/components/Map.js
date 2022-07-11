d6186e10e892be1475b2b195ceccdcd17f378aea
jackmonaco20 <jackmonaco20@yahoo.com>
2022-07-10 21:57:09 -0400

    .

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,21 +1,36 @@
-import GoogleMapReact from "google-map-react";
+import { useState } from 'react'
+import GoogleMapReact from 'google-map-react'
+import LocationMarker from './LocationMarker'
+import LocationInfoBox from './LocationInfoBox'
 
-const Map = ({center,zoom}) => {
+// define constants
+const NATURAL_EVENT_WILDFIRE = 8;
+
+const Map = ({ eventData, center, zoom }) => {
+    const [locationInfo, setLocationInfo] = useState(null)
+
+    const markers = eventData.map((ev, index) => {
+        if(ev.categories[0].id === NATURAL_EVENT_WILDFIRE) {
+            return <LocationMarker key={index} lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} onClick={() => setLocationInfo({ id: ev.id, title: ev.title })} />
+        }
+        return null
+    })
+
     return (
-        <div className = "map">
+        <div className="map">
             <GoogleMapReact
-                boostrapURLKEYS = {{key:
-                'AIzaSyA9UWd9VhJWq_XuCcyMmOFGujOcU1rLSI8'}}
-                defaultCenter = { center }
-                defualtZoom = {zoom}
+                bootstrapURLKeys={{ key: 'AIzaSyA9UWd9VhJWq_XuCcyMmOFGujOcU1rLSI8' }}
+                defaultCenter={ center }
+                defaultZoom={ zoom }
             >
-
+                {markers}
             </GoogleMapReact>
+            {locationInfo && <LocationInfoBox info={locationInfo} />}
         </div>
     )
 }
 
-Map.defualtProps = {
+Map.defaultProps = {
     center: {
         lat: 42.3265,
         lng: -122.8756
